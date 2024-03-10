c4e815a65bc4d1114a01345a000ab5932c37b8f1
Renan <renan.fachin@hotmail.com>
2024-03-09 22:46:30 -0300

    feat: Implementa caso de uso de autenticação

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,10 +1,18 @@
 import { UsersRepository } from "@/repositories/interfaces/users-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { User } from "@prisma/client";
 
 // tipagem de entrada
-interface AuthenticateUseCaseRequest { }
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
 
 // tipagem de resposta
-interface AuthenticateUseCaseResponse { }
+interface AuthenticateUseCaseResponse {
+  user: User
+}
 
 export class AuthenticateUseCase {
   constructor(
@@ -12,7 +20,24 @@ export class AuthenticateUseCase {
   ) { }
 
 
-  async execute() {
-    // autenticação
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    // Buscar o usuário no db pelo email
+    const user = await this.usersRepository.findByEmail(email)
+
+    if(!user){
+      throw new InvalidCredentialsError()
+    }    
+
+    // Comparar a senha salva com a senha do parâmetro
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+
+    if(!doesPasswordMatches){
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user
+    }
   }
 }
