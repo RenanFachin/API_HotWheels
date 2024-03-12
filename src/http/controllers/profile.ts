f4ad05d21641d8478a402751c8041ee14f6786a4
Renan <renan.fachin@hotmail.com>
2024-03-12 17:08:08 -0300

    chore: Adiciona tipagem ao fastify-jwt

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,12 @@
+import chalk from "chalk";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
+  // Buscar o token dentro do header e, caso exista, validar que foi criado pela API
+  await request.jwtVerify()
+
+  console.log(chalk.blueBright('User id: ' + request.user.sub))
 
   return reply.status(200).send()
 }
