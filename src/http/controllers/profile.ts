fb54514b16ee507f41ccd7e60f67ad64db23c1aa
Renan <renan.fachin@hotmail.com>
2024-03-12 17:23:49 -0300

    feat: Implementa lógica para obter os dados do usuário a partir do jwt token

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,3 +1,4 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import chalk from "chalk";
 import { FastifyReply, FastifyRequest } from "fastify";
 
@@ -6,7 +7,19 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
   // Buscar o token dentro do header e, caso exista, validar que foi criado pela API
   await request.jwtVerify()
 
-  console.log(chalk.blueBright('User id: ' + request.user.sub))
+  // console.log(chalk.blueBright('User id: ' + request.user.sub))
 
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub
+  })
+
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
 }
