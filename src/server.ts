561f7834265f701881002a9f07ec6f161602b36d
Renan <renan.fachin@hotmail.com>
2024-03-12 22:21:34 -0300

    deploy: Ajustes para deploy

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import chalk from 'chalk'
 app
   .listen({
     host: '0.0.0.0',
-    port: env.PORT,
+    port: env.PORT ? Number(env.PORT) : 3333,
   })
   .then(() => console.log(chalk.greenBright('🛸 HTTP Server Running!')))
